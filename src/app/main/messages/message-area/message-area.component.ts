6ddb5cf85378d78a52e6ec4a2c6dea1eade30d41
AbhiMophare <77708148+AbhiMophare@users.noreply.github.com>
2023-08-05 19:35:56 +0530

    2 files modified, 5 added

---
+++ src/app/main/messages/message-area/message-area.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-message-area',
+  templateUrl: './message-area.component.html',
+  styleUrls: ['./message-area.component.css'],
+})
+export class MessageAreaComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/main/messages/messages.component.ts
+++ src/app/main/messages/messages.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FooterComponent } from './footer/footer.component';
+import { MessageAreaComponent } from './message-area/message-area.component';
+import { MessageHeaderComponent } from './message-header/message-header.component';
 
 @Component({
   standalone: true,
   selector: 'app-messages',
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.css'],
+  imports: [FooterComponent, MessageAreaComponent, MessageHeaderComponent],
 })
 export class MessagesComponent implements OnInit {
   constructor() {}
