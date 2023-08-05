69a4a1ae0a76ca353f8c211cac81f009eda6f68d
AbhiMophare <77708148+AbhiMophare@users.noreply.github.com>
2023-08-05 15:14:46 +0530

    Initial commit

---
+++ src/app/main/messages/messages.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.css'],
+})
+export class MessagesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
