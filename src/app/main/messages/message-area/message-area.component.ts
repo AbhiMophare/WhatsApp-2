3a85a511e4eff96c8f339dcfe066eb3b79ac692c
AbhiMophare <77708148+AbhiMophare@users.noreply.github.com>
2023-08-06 09:37:51 +0530

    msg inout added

--- src/app/main/messages/message-area/message-area.component.ts
+++ src/app/main/messages/message-area/message-area.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { MsgComponent } from './msg/msg.component';
+import { InoutmsgComponent } from './inoutmsg/inoutmsg.component';
 
 @Component({
   standalone: true,
   selector: 'app-message-area',
   templateUrl: './message-area.component.html',
   styleUrls: ['./message-area.component.css'],
-  inputs: [MsgComponent],
+  imports: [InoutmsgComponent],
 })
 export class MessageAreaComponent implements OnInit {
   constructor() {}
